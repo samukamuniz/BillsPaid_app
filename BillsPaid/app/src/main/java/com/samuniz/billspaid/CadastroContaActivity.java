4bc4c157b7191313cef34045d20dc52d6605783e
Samuel <samuka.muniz@gmail.com>
2017-10-24 20:10:59 -0300

    Activity CadastroConta

---
+++ BillsPaid/app/src/main/java/com/samuniz/billspaid/CadastroContaActivity.java
@@ -0,0 +1,13 @@
+package com.samuniz.billspaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_conta);
+    }
+}
